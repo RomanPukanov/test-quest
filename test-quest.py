5e7cef7f02af7a3945c0c686c8617652629a93dc
RomanPukanov <vorold114@mail.ru>
2023-03-13 20:17:36 +0300

    polnaya huynya

--- test-quest.py
+++ test-quest.py
@@ -12,5 +12,9 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+sadqsd
+asdasda
+sadasdasd
+asdasdasda
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
